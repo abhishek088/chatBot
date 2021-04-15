8eb8caae17e33189da1011142b9dbdc9a029c607
abhishek088 <69878090+abhishek088@users.noreply.github.com>
2021-04-14 22:37:43 -0400

    Update ShawarmaOrder.js

--- ShawarmaOrder.js
+++ ShawarmaOrder.js
@@ -179,6 +179,55 @@ module.exports = class ShwarmaOrder extends Order{
       </body>
           
       `);
+        renderForm2(){
+      // your client id should be kept private
+      const sClientID = process.env.SB_CLIENT_ID || 'Ab-gDapUoAwuu5GIHm1ccqX59hypCpbabxlTPearxdQYJOd-M7D_z7KXYBappwb4y2zwsKHvn17-UtUq'
+      return(`
+      <!DOCTYPE html>
+  
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1"> <!-- Ensures optimal rendering on mobile devices. -->
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" /> <!-- Optimal Internet Explorer compatibility -->
+      </head>
+      
+      <body>
+        <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+        <script
+          src="https://www.paypal.com/sdk/js?client-id=${sClientID}"> // Required. Replace SB_CLIENT_ID with your sandbox client ID.
+        </script>
+        Thank you for your order
+        <div id="paypal-button-container"></div>
+  
+        <script>
+          paypal.Buttons({
+              createOrder: function(data, actions) {
+                // This function sets up the details of the transaction, including the amount and line item details.
+                return actions.order.create({
+                  purchase_units: [{
+                    amount: {
+                      value: 21
+                    }
+                  }]
+                });
+              },
+              onApprove: function(data, actions) {
+                // This function captures the funds from the transaction.
+                return actions.order.capture().then(function(details) {
+                  // This function shows a transaction success message to your buyer.
+                  $.post(".", details, ()=>{
+                    window.open("", "_self");
+                    window.close(); 
+                  });
+                });
+              }
+          
+            }).render('#paypal-button-container');
+          // This function displays Smart Payment Buttons on your web page.
+        </script>
+      
+      </body>
+          
+      `);
   
     }
 }
